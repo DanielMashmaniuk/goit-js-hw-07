6343b2a212ea7b3ac7d70134affb9547f2bac812
DanielMashmaniuk <82703760+DanielMashmaniuk@users.noreply.github.com>
2021-09-23 17:27:35 +0300

    1

---
+++ js/task-04.js
@@ -0,0 +1,8 @@
+const range = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+text.style.fontSize = `${range.value}px`;
+
+range.addEventListener('input',(event)=>{
+const valueOfRange = event.currentTarget.value;
+text.style.fontSize = `${valueOfRange}px`;
+});
